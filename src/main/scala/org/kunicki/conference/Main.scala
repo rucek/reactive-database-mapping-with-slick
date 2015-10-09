bc8e79da2ceb7d7a5abfb83da4b11424beae3808
Jacek Kunicki <jacek@kunicki.org>
2015-10-10 00:37:45 +0200

    Database schema

---
+++ src/main/scala/org/kunicki/conference/Main.scala
@@ -0,0 +1,30 @@
+package org.kunicki.conference
+
+import org.kunicki.conference.db.DatabaseSchema
+import slick.driver.H2Driver.api._
+import slick.jdbc.meta.MTable
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.util.Success
+
+object Main extends App with DatabaseSchema {
+
+  val db = Database.forConfig("h2")
+
+  Await.ready(createSchemaIfNotExists(), Duration.Inf)
+
+  def createSchemaIfNotExists(): Future[Unit] = {
+    db.run(MTable.getTables).flatMap {
+      case tables if tables.isEmpty =>
+        val schema = rooms.schema ++ speakers.schema ++ talks.schema ++ talksSpeakers.schema ++ votes.schema
+        db.run(schema.create).andThen {
+          case Success(_) => println("Schema created")
+        }
+      case tables if tables.nonEmpty =>
+        println("Schema already exists")
+        Future.successful()
+    }
+  }
+}
