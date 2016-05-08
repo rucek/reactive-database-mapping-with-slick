2e8d7b319741b377d9826bc544415daab2ee8442
Jacek Kunicki <jacek@kunicki.org>
2016-05-08 23:22:57 +0200

    Database mappings

--- src/main/scala/org/kunicki/conference/Main.scala
+++ src/main/scala/org/kunicki/conference/Main.scala
@@ -1,16 +1,5 @@
 package org.kunicki.conference
 
-import org.kunicki.conference.db.{DatabaseSchema, InitialData}
-import slick.driver.H2Driver.api._
+object Main extends App {
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-
-object Main extends App with DatabaseSchema with InitialData with Magic {
-
-  val db = Database.forConfig("h2")
-
-  private val future = createSchemaIfNotExists.flatMap(_ => insertInitialData())
-  Await.ready(future, Duration.Inf)
 }
--- src/main/scala/org/kunicki/conference/db/InitialData.scala
+++ src/main/scala/org/kunicki/conference/db/InitialData.scala
@@ -1,41 +1,5 @@
 package org.kunicki.conference.db
 
-import org.kunicki.conference.domain.{Room, Speaker, Talk, TalkSpeaker}
-import slick.driver.H2Driver.api._
-
-import scala.concurrent.Future
-
 trait InitialData {
-  self: DatabaseSchema =>
 
-  def db: Database
-
-  def insertInitialData(): Future[Unit] = {
-    db.run(DBIO.seq(
-      votes.delete, talksSpeakers.delete, talks.delete, speakers.delete, rooms.delete,
-
-      rooms += Room(1, "Room 1"),
-      rooms += Room(2, "Room 2"),
-
-      speakers ++= Seq(
-        Speaker(1, "Speaker 1"),
-        Speaker(2, "Speaker 2"),
-        Speaker(3, "Speaker 3")
-      ),
-
-      talks ++= Seq(
-        Talk(1, 1, "Talk 1"),
-        Talk(2, 1, "Talk 2"),
-        Talk(3, 2, "Talk 3"),
-        Talk(4, 2, "Talk 4")
-      ),
-
-      talksSpeakers ++= Seq(
-        TalkSpeaker(1, 3),
-        TalkSpeaker(2, 1),
-        TalkSpeaker(3, 2),
-        TalkSpeaker(4, 3)
-      )
-    ))
-  }
 }
